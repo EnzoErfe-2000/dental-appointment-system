addd64374b6d07ef645638e8d20b18218e330330
enzoerfe2000 <enzoerfe2000@gmail.com>
2024-06-10 15:51:42 +0800

    Created new css file and edited HTML for index.php

--- header.php
+++ header.php
@@ -1,36 +1,61 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 <header>
+    <div class="mobile-nav-links" id="mobile-nav-links">
+        <a href="#schedules.php">Schedules</a>
+        <a href="#check-appointment.php">Check My Appointments</a>
+        <a href="#login.php">Login</a>
+    </div>
+    
     <a class="logo" href="index.php"><img src="images/logo.jpg" alt="logo"></a>
-    <nav>
-    <ul class="nav__links">
-    <li><a href="index.php">Home</a></li>
-    <?php if(!isset($_SESSION['username'])){?> 
-	 <li><a href="registration.php">Sign up</a></li>	
-     <li><a class="cta" href="login.php">Login</a></li>
-    <?php }?>
-	<?php if(isset($_SESSION['username'])){
-        if($_SESSION['role'] == 'admin')
-        {
-            echo "<li><a href='addclinic.php'>Add Clinic</a></li>";
-            echo "<li><a href='adddentist.php'>Add Dentist</a></li>";
+    
+    <a href="javascript:void(0);" class="burger-icon" onclick="myFunction()">
+        <i class="fa fa-bars"></i>
+    </a>
+
+    <script>
+    function myFunction() {
+        const navLinks = document.getElementById('mobile-nav-links');
+        
+        if (navLinks.style.display === "block") {
+            navLinks.style.display = "none";
         }
         else {
-        if($_SESSION['role'] == 'patient')
-        {
-            echo "<li><a href='clinics.php'>Clinics </a></li>";
-        }
-    ?> 	
-	 
-    <li><a href="appointments.php">Appointments</a></li>
-    <li><a href="pastappointments.php">Past Appointments</a></li>
-    <?php if($_SESSION['role'] == 'patient'){
-        echo "<li><a href='updateaccount.php'>Update Account</a></li>";
-    }}?>
-    
-    <li><a href="index.php?logout='1'">Logout</a> </li>	
-    <?php }?>
-    
+            navLinks.style.display = "block";
+        };
+    }
+    </script>
 
-                   
+    <nav>
+        <ul class="nav__links">
+            <li><a href="index.php">Home</a></li>
+            <?php if(!isset($_SESSION['username'])){?> 
+            <!-- <li><a href="registration.php">Sign up</a></li>	 -->
+            <li><a href="schedules.php">Schedules</a></li>	
+            <li><a href="check-appointment.php">Check My Appointment</a></li>
+            <li><a class="cta" href="login.php">Login</a></li>
+            <?php }?>
+            <?php if(isset($_SESSION['username'])){
+                if($_SESSION['role'] == 'admin')
+                {
+                    echo "<li><a href='addclinic.php'>Add Clinic</a></li>";
+                    echo "<li><a href='adddentist.php'>Add Dentist</a></li>";
+                }
+                else {
+                if($_SESSION['role'] == 'patient')
+                {
+                    echo "<li><a href='clinics.php'>Clinics </a></li>";
+                }
+            ?> 	
+            
+            <li><a href="appointments.php">Appointments</a></li>
+            <li><a href="pastappointments.php">Past Appointments</a></li>
+            <?php if($_SESSION['role'] == 'patient'){
+                echo "<li><a href='updateaccount.php'>Update Account</a></li>";
+            }}?>
+        
+            <li><a href="index.php?logout='1'">Logout</a> </li>	
+            <?php }?>           
                 </ul>
             </nav>
            
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 $db=mysqli_connect('localhost','root','','dcms') or die("could not connect to database");
 if(isset($_GET['logout'])){
-    
+
     session_destroy();
     unset($_SESSION['username']);
     unset($_SESSION['role']);
@@ -19,53 +19,67 @@ if(isset($_GET['logout'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="styles-1.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:500&display=swap" rel="stylesheet">
 </head>
-	
+
 	<body>
-    <?php require_once("header.php");?>
+        <?php require_once("header.php");?>
         <div id="mobile__menu" class="overlay">
             <a class="close">&times;</a>
             <div class="overlay__content">
                 <a href="index.php">Home</a>
-                <a href="registration.php">Sign up</a>
+                <!-- <a href="registration.php">Sign up</a> -->
+                <a href="schedules.php">Schedules</a>
+                <a href="check-appointment.php">My Appointment</a>
                 <a href="login.php">Login</a>
             </div>
         </div>
-		<div class=homemain>
-		<div class="caption">
-		<?php if(isset($_SESSION['username'])):?>
-	
-    <h2>Welcome <strong><?php echo $_SESSION['username'] ; ?> </strong>
-    </h2>
-    <?php endif; ?>
-	<br>
-	<br>
-	<br>
-		<a class="w">We prioritise your</a>
-		<h1>NEW SMILE</h1>
-        <!--<button class="ta" href="#">Read more</button>-->
-		</div>
-		</div>
-		<?php if(isset($_SESSION['success'])):?>
-        <div>
-            <h3>
-            <?php
-            echo $_SESSION['success'];
-            unset($_SESSION['success'])
-            ?>
-            </h3>
+        <div class=homemain>
+            
+
+        <div class="container">
+            <div class="column">
+                <div class="sub-div">
+                    <div class="caption">
+                    <?php if(isset($_SESSION['username'])):?>
+
+                        <h2>Welcome <strong><?php echo $_SESSION['username'] ; ?> </strong>
+                    </h2>
+                    <?php endif; ?>
+                        <a class="w">We prioritise your</a>
+                        <h1>NEW SMILE</h1>
+                        <!--<button class="ta" href="#">Read more</button>-->    
+                    </div>
+                </div>
+                <?php if(isset($_SESSION['success'])):?>
+                <div>
+                    <h3>
+                    <?php
+                    echo $_SESSION['success'];
+                    unset($_SESSION['success'])
+                    ?>
+                    </h3>
+                </div>
+                <?php endif;?>
+            </div>
+
+            <div class="column">
+                <div class="sub-div">
+                    <div class="main">
+                        <div>
+                            <a><img src="images/main.jpg" alt="main"></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="sub-div">
+                    <div>
+                        Book an appointment today!
+                    </div>
+                </div>
+            </div>
         </div>
-    <?php endif;?>
-    
-		<div>
-		<a class="main"><img src="images/main.jpg" alt="main"></a>
-		</div>
-        <div>
-            Book an appointment today!
-        </div>
-        <script type="text/javascript" src="mobile.js"></script>  
-</center>
+        <script type="text/javascript" src="mobile.js"></script>
+<!-- </center> -->
 </body>
 </html>
